bcd53ea55144997d8a318fe74332362446cb04a8
Cleber Lima <thiagocaffaro223@gmail.com>
2023-12-29 18:32:15 -0300

    hahahhaha

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Cards from './pages/cards.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/cards",
-    element: <Cards />,
-  },
-])
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import Cards from './pages/cards.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter({
+  basename: process.env.NODE_ENV === 'production' ? '/carta' : '',
+  routes: [
+    {
+      path: '/',
+      element: <App />,
+    },
+    {
+      path: '/cards',
+      element: <Cards />,
+    },
+  ],
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
-)
+  <RouterProvider router={router} />
+);
