2aa5f45404d5be4bcddbabde251a9c1484e7a040
Cleber Lima <thiagocaffaro223@gmail.com>
2023-12-29 18:41:04 -0300

    bro

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,20 @@ import './index.css';
 import Cards from './pages/cards.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/cards',
+    element: <Cards />,
+  },
+];
+
 const router = createBrowserRouter({
   basename: process.env.NODE_ENV === 'production' ? '/carta' : '',
-  routes: [
-    {
-      path: '/',
-      element: <App />,
-    },
-    {
-      path: '/cards',
-      element: <Cards />,
-    },
-  ],
+  routes: routes, // Certifique-se de passar o array de rotas corretamente
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
